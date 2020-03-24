b28bfb370870c80c48d380c88e0f9c5eae2a932a
Jaime385 <47996736+jaime385@users.noreply.github.com>
2020-03-24 17:49:45 -0600

    Se realizaron pruebas con la libreria parallax.js

--- func.js
+++ func.js
@@ -3,7 +3,8 @@ console.log('Primer linea javascript');
 async function getmeme() {
     const response = await fetch('https://api.imgflip.com/get_memes'); // this is a string so we want to convert it to json format.
     const data = await response.json();
-    const meme = data.data.memes[between(1, 99)].url;
+    const ramdom = between(0, 99); // La api call contiene 100 memes.
+    const meme = data.data.memes[ramdom].url;
     document.getElementById('meme').src = meme;
 }
 /* Returns a random number between min (inclusive) and max (inclusive)*/
@@ -13,3 +14,12 @@ function between(min, max) {
     )
 }
 getmeme();
+
+/*const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'yellowgreen';
+ctx.fillRect(10, 10, 150, 100);*/
+
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene);
