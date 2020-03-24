7932a309304b1695a91854bf8cf63c0486b113c9
Jaime385 <47996736+jaime385@users.noreply.github.com>
2020-03-24 14:24:00 -0600

    1. Se anadio una imagen tipo meme a index.html. 2. Se centro la imagen en styles.css 3. Se usa fetch para obtener una imagen tipo meme en func.js

--- func.js
+++ func.js
@@ -1,2 +1,15 @@
 console.log('Primer linea javascript');
-console.log('Segunda linea.');
+/* La funcion get meme y between simplemente realizan un api call obteniendo una base para meme.*/
+async function getmeme() {
+    const response = await fetch('https://api.imgflip.com/get_memes'); // this is a string so we want to convert it to json format.
+    const data = await response.json();
+    const meme = data.data.memes[between(1, 99)].url;
+    document.getElementById('meme').src = meme;
+}
+/* Returns a random number between min (inclusive) and max (inclusive)*/
+function between(min, max) {
+    return Math.floor(
+        Math.random() * (max - min + 1) + min
+    )
+}
+getmeme();
